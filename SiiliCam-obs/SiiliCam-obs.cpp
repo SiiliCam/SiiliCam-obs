6f34252d16c93d0f7f105bae326c7799ebad9ce2
Simo Rautiainen <simo.rautiainen@gmail.com>
2023-09-24 08:57:11 +0300

    initial commit

---
+++ SiiliCam-obs/SiiliCam-obs.cpp
@@ -0,0 +1,12 @@
+﻿// SiiliCam-obs.cpp : Defines the entry point for the application.
+//
+
+#include "SiiliCam-obs.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
